8503470338b0ffb577bc5c69992425b650b4ada1
DefaultUserNm <amkabakov@gmail.com>
2023-04-26 01:33:28 +0300

    feat: add tests logs

--- spring-app/src/test/groovy/ApplicationIT.groovy
+++ spring-app/src/test/groovy/ApplicationIT.groovy
@@ -1,14 +1,17 @@
-
-
 import org.springframework.boot.test.context.SpringBootTest
+import ru.home.application.Application
 import spock.lang.Specification
-
 
 /*
  * @created 25.04.2023
  * @author alexander
  */
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = [
+                Application
+        ]
+)
 class ApplicationIT extends Specification {
 
     def "verify context starts"() {
