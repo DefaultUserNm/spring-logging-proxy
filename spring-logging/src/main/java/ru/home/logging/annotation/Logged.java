bc07b68be7bc4e255db90f2a58832967d1e8b05e
DefaultUserNm <amkabakov@gmail.com>
2023-04-26 00:51:31 +0300

    fix: annotation target

--- spring-logging/src/main/java/ru/home/logging/annotation/Logged.java
+++ spring-logging/src/main/java/ru/home/logging/annotation/Logged.java
@@ -4,11 +4,16 @@ import ru.home.logging.model.ProxyMode;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 
 /*
  * @created 13.04.2023
  * @author alexander
  */
+@Target({ TYPE, METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Logged {
     ProxyMode proxyMode() default ProxyMode.DEFAULT;
